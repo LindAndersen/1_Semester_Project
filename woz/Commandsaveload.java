49e2bf9d1a960785776892fb20fd8564c5941125
Matt-G-K <146090539+Matt-G-K@users.noreply.github.com>
2023-10-22 14:36:37 +0200

    Add files via upload
    Save Load Command

---
+++ woz/Commandsaveload.java
@@ -0,0 +1,71 @@
+import java.io.*;
+import java.util.*;
+
+class Commandsaveload extends BaseCommand implements Command {
+    private final String fileName; // This variable stores the name of the file where we save and read variables.
+    private final Map<String, String> variables; // Use a LinkedHashMap to preserve the order
+
+    // Constructor to initialize the class with a file name.
+    public Commandsaveload(String fileName) {
+        this.fileName = fileName;
+        variables = new LinkedHashMap<>();
+    }
+
+    // Method to save a variable and its value to the text file.
+    public void save(String variableName, String variableValue) {
+        // Read existing variables, overwrite the file, and then add the new variable.
+        variables.put(variableName, variableValue);
+
+        try (PrintWriter writer = new PrintWriter(new FileWriter(fileName, false))) {
+            for (String key : variables.keySet()) {
+                writer.println(key + "=" + variables.get(key));
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    // Method to read variables from the text file and store them in a map.
+    public Map<String, String> load() {
+        Map<String, String> variables = new LinkedHashMap<>();
+
+        try (BufferedReader reader = new BufferedReader(new FileReader(fileName)) ) {
+            // Read lines from the file and split them into variable name and value.
+            reader.lines().map(line -> line.split("=", 2))
+                    .filter(parts -> parts.length == 2)
+                    .forEach(parts -> variables.put(parts[0], parts[1]));
+        } catch (IOException e) {
+            e.printStackTrace();
+            // Handle any errors that occur during file reading, such as if the file doesn't exist.
+        }
+
+        return variables;
+    }
+
+    @Override
+    public void execute (Context context, String command, String[] parameters) {
+
+        Commandsaveload Commandsaveload = new Commandsaveload("variables.txt");
+
+        switch(command) {
+
+            case "save":
+
+
+                // Save variables
+                Commandsaveload.save("name", "Tom Smith");
+                Commandsaveload.save("age", "34");
+                Commandsaveload.save("city", "New Amsterdam");
+                Commandsaveload.save("city status", "Polluted");
+                Commandsaveload.save("lake status", "5 Trash");
+
+                break;
+
+            case "load":
+                // Read and print variables
+                Map<String, String> savedVariables = Commandsaveload.load();
+                savedVariables.forEach((key, value) -> System.out.println(key + " = " + value));
+                break;
+        }
+    }
+}
