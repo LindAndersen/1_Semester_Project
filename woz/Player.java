248f8e5a0762cf8a7d809a10694adc32ecd9d4ed
LindAndersen <andersen.flan@gmail.com>
2023-10-29 20:24:09 +0100

    Bug fix and added msg for player.addToInventory

--- woz/Player.java
+++ woz/Player.java
@@ -22,6 +22,7 @@ class Player {
 
     void addToInventory(String name, int amount){
         //calls an add-method in Inventory class
+        System.out.printf("Du har tilføjet %d %s til din inventar%n", amount, name);
     }
 
     void removeFromInventory(String name, int amount){
