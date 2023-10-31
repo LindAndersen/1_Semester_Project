3a0a626b133692c699706cd54628eadbed7cfafc
Matt-G-K <146090539+Matt-G-K@users.noreply.github.com>
2023-10-31 13:06:45 +0100

    Create Commandsaveloadobject.java

---
+++ woz/Commandsaveloadobject.java
@@ -0,0 +1,45 @@
+import java.io.*;
+
+public class ObjectFileManager {
+
+    // Method to save an object to a file
+    public static void saveObjectToFile(Object obj, String filePath) {
+        try (FileOutputStream fileOut = new FileOutputStream(filePath);
+             ObjectOutputStream objectOut = new ObjectOutputStream(fileOut)) {
+            objectOut.writeObject(obj);
+            System.out.println("Object saved to " + filePath);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    // Method to load an object from a file
+    public static Object loadObjectFromFile(String filePath) {
+        Object obj = null;
+        try (FileInputStream fileIn = new FileInputStream(filePath);
+             ObjectInputStream objectIn = new ObjectInputStream(fileIn)) {
+            obj = objectIn.readObject();
+            System.out.println("Object loaded from " + filePath);
+        } catch (IOException | ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+        return obj;
+    }
+
+    public static void main(String[] args) {
+        // Example of saving and loading an object
+        // Replace 'YourObject' with the class of the object you want to save/load.
+        YourObject objectToSave = new YourObject("Example Data");
+
+        // Save the object to a file
+        saveObjectToFile(objectToSave, "objectData.ser");
+
+        // Load the object from the file
+        YourObject loadedObject = (YourObject) loadObjectFromFile("objectData.ser");
+
+        // Verify the loaded object
+        if (loadedObject != null) {
+            System.out.println("Loaded Data: " + loadedObject.getData());
+        }
+    }
+}
