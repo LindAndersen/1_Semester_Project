083decf2cc96978804865505368ca51df1297431
Matt-G-K <146090539+Matt-G-K@users.noreply.github.com>
2023-11-20 10:48:35 +0100

    Delete woz directory

--- woz/CommandUnknown.java
+++
@@ -1,9 +0,0 @@
-/* Fallback for when a command is not implemented
- */
-
-class CommandUnknown extends BaseCommand implements Command {
-  @Override
-  public void execute (Context context, String command, String parameters[]) {
-    System.out.println("\nHovsa, jeg tror desværre ikke, at jeg forstår '"+command+"' 😕");
-  }
-}
