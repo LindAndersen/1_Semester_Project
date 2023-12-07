6a039e078dcb58eca3ad855faa895eacbd8847a5
Asta Knudsen <asta.strachottak@gmail.com>
2023-12-07 08:21:54 +0100

    player 0 penge i stedet for 1020

--- genbrugsstation/src/main/java/com/genbrugsstation/Player.java
+++ genbrugsstation/src/main/java/com/genbrugsstation/Player.java
@@ -15,7 +15,7 @@ class Player implements Serializable {
         this.name = name;
         xp = 0;
         lvl = 0;
-        money = 1020;
+        money = 0;
         inventory = new Inventory();
         resetInventory();
         buyCount = 0;
